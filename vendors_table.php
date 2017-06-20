0f06d21ce782141f81d2436c1d0b63793d752ece
Iaroslav Shchevych <sche.contakt@gmail.com>
2017-06-20 21:25:17 +0200

    Update vendors_table.php

--- vendors_table.php
+++ vendors_table.php
@@ -1,4 +1,22 @@
-<?php
+<form action="add_vendor.php" method="POST">
+			Dodać do listy dostawców: <br/> 
+			Wprowadż nazwę: <input type="text" name="nazwa" required/><br/>
+			Wprowadż kontakt: <input type="text" name="kontakt" required/><br/>
+			Wprowadż kraj: <input type="text" name="kraj" required/><br/>
+		<input type="submit" value="Dodaj dostawcę"/>
+		<h2 align="center">Lista dostawców</h2>
+		<table border="1px" width="100%">
+			<tr>
+				<th>ID</th>
+				<th>Nazwa</th>
+				<th>Kontakt</th>
+				<th>Kraj</th>
+				<th>Edytuj</th>
+				<th>Usuń</th>
+				
+			</tr>
+			<?php
+				include "db_connect.php";
 				$query = mysql_query("SELECT * FROM dostawca");
 				while($row = mysql_fetch_array($query))
 				{
@@ -12,3 +30,4 @@
 					Print "</tr>";
 				}
 			?>
+		</table>
